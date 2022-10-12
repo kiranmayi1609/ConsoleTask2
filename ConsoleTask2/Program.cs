308140c8427d969a918cb69949286e167a8d8b7b
kiranmayi <kiranmayi1609@gmail.com>
2022-10-12 12:11:00 +0200

    Add project files.

---
+++ ConsoleTask2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleTask2
+{
+    class program
+    {
+        static void Main(string[] args )
+        {
+            string[] Names = { "Kurt", "Kim", "Ali", "Annatte" };
+            string[] copy=new string[Names.Length];
+            for (int i = 0; i < copy.Length; i++)
+           
+                copy[i] = Names[i];
+            
+            Console.WriteLine(copy[0]);
+            
+        }
+    }
+}
